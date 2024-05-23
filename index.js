99c0e11cb79d1340649bf6a1e52889097506a19b
ammarAlaliS <ammaralialiwa@gmail.com>
2024-05-22 19:56:37 -0600

    se agrego la ruta raiz

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const dbConnection = require('./config/dbConnection');
-const app = express();
-const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000;
+const dotenv = require('dotenv').config();
 const authRouter = require('./routes/authRoute');
 const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middleawares/errorHandle');
-
 const cookieParser = require('cookie-parser');
-dbConnection()
+const cors = require('cors'); 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dbConnection();
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true
+}));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/ObbaraMarket', authRouter)
+// Ruta para manejar la raíz
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido a ObbaraMarket_Backend!');
+});
+
+app.use('/api/ObbaraMarket', authRouter);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, ()=>{
-    console.log(`server is running at PORT ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is running at PORT ${PORT}`);
 });
