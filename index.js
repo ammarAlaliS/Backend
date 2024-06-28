c318a469ae0982c745bdf8a8725a6bd4eddbe9f3
ammarAlaliS <ammaralialiwa@gmail.com>
2024-06-28 03:51:08 -0600

    permitir solicitudes del cliente netlify

--- index.js
+++ index.js
@@ -6,47 +6,60 @@ const authRouter = require('./routes/authRoute');
 const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middleawares/errorHandle');
 const cookieParser = require('cookie-parser');
-const cors = require('cors'); 
-const setupSwaggerDocs = require('./swaggerDocs'); 
-const { initialize } = require('./socketLogic');  // Importar la función initialize
+const cors = require('cors');
+const setupSwaggerDocs = require('./swaggerDocs');
+const { initialize } = require('./socketLogic');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Conexión a la base de datos
 dbConnection();
 
-app.use(cors({
-    origin: '*',
+// Configuración de CORS para permitir solicitudes desde el origen específico
+const corsOptions = {
+    origin: 'https://obbaramarket-dashbord.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true
-}));
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Habilita el intercambio de cookies a través de las solicitudes CORS
+};
+
+app.use(cors(corsOptions));
+
+// Middleware para analizar cuerpos de solicitud entrantes
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Middleware para manejar cookies
 app.use(cookieParser());
 
-// Ruta para manejar la raíz
+// Ruta para manejar la raíz del servidor
 app.get('/', (req, res) => {
-  res.send('¡Bienvenido a ObbaraMarket_Backend!');
+    res.send('¡Bienvenido a ObbaraMarket_Backend!');
 });
 
+// Rutas para la autenticación
 app.use('/api/ObbaraMarket', authRouter);
 
-// Setup Swagger documentation
+// Configuración de la documentación Swagger
 setupSwaggerDocs(app, PORT);
 
 // Middleware para manejar rutas no encontradas
 app.use(notFound);
+
 // Middleware para manejar errores
 app.use(errorHandler);
 
+// Creación del servidor HTTP con Express
 const server = http.createServer(app);
 
-// Inicializar socket.io
+// Inicialización de socket.io
 initialize(server);
 
+// Inicio del servidor en el puerto especificado
 server.listen(PORT, () => {
-  console.log(`server is running at PORT ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
+// Exportación del servidor para pruebas unitarias u otros propósitos
 module.exports = server;
