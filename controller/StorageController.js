59e70c5a5287832eefd3abbea07798ea30b47fef
Nestor Gonzalez <nestorgt37@gmail.com>
2024-06-28 20:46:02 -0600

    Update prodcucto controller
    Fix date bug and make storage logic

---
+++ controller/StorageController.js
@@ -0,0 +1,109 @@
+const asyncHandler = require("express-async-handler");
+const Blog = require("../models/Blog");
+const { Storage } = require("@google-cloud/storage");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+// Configuración de Google Cloud Storage
+const storage = new Storage({
+  projectId: process.env.GCLOUD_PROJECT_ID,
+  credentials: {
+    private_key: process.env.GCLOUD_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.GCLOUD_CLIENT_EMAIL,
+  },
+});
+
+const bucketName = "quickcar"; // Nombre de tu bucket en Google Cloud Storage
+
+// Configurar el almacenamiento en memoria para Multer
+const memoryStorage = multer.memoryStorage();
+const upload = multer({ storage: memoryStorage });
+
+// Función para subir una imagen a Google Cloud Storage
+const uploadImageToStorage = async (file, altText) => {
+  const blob = storage
+    .bucket(bucketName)
+    .file(uuidv4() + "_" + file.originalname);
+  const blobStream = blob.createWriteStream({ resumable: false });
+
+  return new Promise((resolve, reject) => {
+    blobStream.on("error", (err) => {
+      reject(err);
+    });
+
+    blobStream.on("finish", () => {
+      const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+      resolve({ url: publicUrl, alt: altText });
+      console.log(`Imagen subida: ${publicUrl}`);
+    });
+
+    blobStream.end(file.buffer);
+  });
+};
+
+// Middleware para manejar las imágenes y los datos del formulario
+const handleFormData = (req, res, next) => {
+  upload.fields([{ name: "blog_image_url", maxCount: 5 }])(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res
+        .status(400)
+        .json({ message: "Error uploading images", error: err });
+    } else if (err) {
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: err });
+    }
+    next();
+  });
+};
+
+// Middleware para manejar las imágenes y los datos del formulario de productos
+const handleProductFormData = (req, res, next) => {
+  upload.fields([{ name: "product_image_url", maxCount: 5 }])(
+    req,
+    res,
+    (err) => {
+      if (err instanceof multer.MulterError) {
+        return res
+          .status(400)
+          .json({ message: "Error uploading images", error: err });
+      } else if (err) {
+        return res
+          .status(500)
+          .json({ message: "Internal server error", error: err });
+      }
+      next();
+    }
+  );
+};
+
+// Función para procesar las imágenes de un campo específico
+const processImages = async (files) => {
+  let imageUrls = [];
+  if (files && files.length > 0) {
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const imageUrl = await uploadImageToStorage(file, file.originalname);
+      console.log(`URL de imagen subida: ${imageUrl}`);
+      imageUrls.push(imageUrl);
+    }
+  }
+  return imageUrls;
+};
+
+// Función para eliminar una imagen de Google Cloud Storage
+const deleteImageFromStorage = async (fileName) => {
+  try {
+    await storage.bucket(bucketName).file(fileName).delete();
+  } catch (error) {
+    console.error(`No se encontro la imagen: ${error.message}`);
+  }
+};
+
+module.exports = {
+  uploadImageToStorage,
+  handleFormData,
+  processImages,
+  handleProductFormData,
+  deleteImageFromStorage,
+};
