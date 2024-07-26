1926d4b8cd186918e3661165861b81ed1b76d29c
Nestor Gonzalez <nestorgt37@gmail.com>
2024-07-25 21:22:07 -0600

    Sending quickcar geolocation socket logic

--- socketLogic.js
+++ socketLogic.js
@@ -1,11 +1,21 @@
-const socketIo = require('socket.io');
+const socketIo = require("socket.io");
 
 let io;
 
 function initialize(server) {
   io = socketIo(server);
-  io.on('connection', (socket) => {
-    console.log('Un cliente se ha conectado');
+  io.on("connection", (socket) => {
+    console.log("Un cliente se ha conectado");
+
+    //Unirse a una sala
+    io.on("joinDriverRoom", (roomId) => {
+      socket.join(roomId);
+    });
+
+    // Emitir ubicacion a una sala
+    socket.on("sendDriverLocation", ({ room, driverLocation }) => {
+      io.to(room).emit("reciveDriverLocation", driverLocation);
+    });
   });
 }
 
@@ -14,11 +24,11 @@ function emitEvent(eventName, eventData) {
     io.emit(eventName, eventData);
     console.log(`Evento "${eventName}" emitido con los datos:`, eventData);
   } else {
-    console.error('Socket.io is not initialized');
+    console.error("Socket.io is not initialized");
   }
 }
 
 module.exports = {
   initialize,
-  emitEvent
+  emitEvent,
 };
